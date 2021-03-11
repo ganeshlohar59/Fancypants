298a5ed5a90cc99b5dc93d51769335a30c4c28d9
Ganesh Lohar <38778334+ganeshlohar59@users.noreply.github.com>
2021-03-12 00:12:15 +0530

    Added basic Routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import "./App.css";
 // Components
 import Homepage from "./pages/homepage/homepage.component";
 
+// Router
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+const MensProductsPage = () => (
+  <div>
+    <h1>Mens Products</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route exact={true} path="/" component={Homepage} />
+          <Route path="/mens" component={MensProductsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Router
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
