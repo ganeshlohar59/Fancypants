c96bc8abc2d9e7c2d58d2ff6eb136021f254e3a5
Ganesh Lohar <38778334+ganeshlohar59@users.noreply.github.com>
2021-05-09 14:16:26 +0530

    :wrench:

--- src/components/stripe-pay-button/stripe-pay-button.component.jsx
+++ src/components/stripe-pay-button/stripe-pay-button.component.jsx
@@ -5,24 +5,43 @@ import StripeCheckout from "react-stripe-checkout";
 
 import { convertToCurrencyString } from "../../utils/string-formatter.utils";
 
+// Constants
+const CURRENCY = "INR";
+const STRIPE_PUBLISHABLE_KEY =
+  "pk_test_51IWyL5SHddtkqiNU1vEtwdMqjE3Cfp1cCBIqvzogZ6T3hlEL9hBdHJBQKL6NJXMWRCuYHKf4gtBj2hJEfEdcvkf8006YCOAHcT";
+
 const StripePayButton = ({ price }) => {
   const stripePrice = price * 100;
-  const publishableKey =
-    "pk_test_51IWyL5SHddtkqiNU1vEtwdMqjE3Cfp1cCBIqvzogZ6T3hlEL9hBdHJBQKL6NJXMWRCuYHKf4gtBj2hJEfEdcvkf8006YCOAHcT";
-
   const currencyString = convertToCurrencyString(price, "INR");
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful, your order is placed ");
+    console.log("Payment Successfull");
+    // axios({
+    //   url: "payment",
+    //   method: "post",
+    //   data: {
+    //     amount: stripePrice,
+    //     token,
+    //   },
+    // })
+    //   .then((response) => {
+    //     console.log(response.json());
+    //     alert("Payment Successfull");
+    //   })
+    //   .catch((error) => {
+    //     alert(
+    //       "Something went wrong with your payment! check you card deails and try again."
+    //     );
+    //     console.log(JSON.parse(error));
+    //   });
   };
 
   return (
     <StripeCheckout
       className="pay-button"
       price={stripePrice}
-      currency="INR"
-      stripeKey={publishableKey}
+      currency={CURRENCY}
+      stripeKey={STRIPE_PUBLISHABLE_KEY}
       panelLabel="Pay Now"
       name="Fancypants"
       billingAddress
